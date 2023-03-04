d821a91217b0e875a797dad6d51273a331b12a38
Anatolii Melnyk <anatolii.melnyk@megogo.net>
2023-03-04 19:59:39 +0200

    some fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,7 +9,7 @@ const Index: NextPage = () => {
         <br /> the software developer.
       </h1>
       <p className="mb-[1.2em] text-[1.3em]">
-        👨‍💻 I have been working in web development for over 3 years and have extensive experience using technologies such as React, Redux, TypeScript and many other web technologies.
+        👨‍💻 I have been working in web development for over 3 years and have extensive experience using technologies such as React, Redux, TypeScript, and many others web technologies.
       </p>
       <p className="mb-[1em] text-[1.1em]">
       🚀 I am constantly engaged in self-improvement because I believe that learning is the key to success in the field of programming. I always stay up to date with new products and trends in the industry to keep up with the latest technologies and approaches.
